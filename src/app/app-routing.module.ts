ccf8e8b84b6dfb15123975c021a6e334cb5b831e
luisaglez <luisaglez@hotmail.es>
2019-03-26 14:30:39 -0600

    inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{InicioComponent} from'./components/inicio/inicio.component';
+import{NoticiasComponent} from'./components/noticias/noticias.component';
+import {DescuentosComponent } from './components/descuentos/descuentos.component';
+import{EventosComponent} from'./components/eventos/eventos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: InicioComponent},
+  {path:'noticias', component: NoticiasComponent},
+  {path:'descuentos',component:DescuentosComponent},
+  {path:'eventos', component:EventosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { AlumnosListComponent } from './components/alumnos/alumnos-list/alumnos-list.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import { DescuentosComponent } from './components/descuentos/descuentos.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AlumnosComponent,
+    AlumnosListComponent,
+    NoticiasComponent,
+    DescuentosComponent,
+    EventosComponent,
+    InicioComponent,  ],
   imports: [
     BrowserModule,
     AppRoutingModule
